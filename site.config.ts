5ce327121a27e80ade2bc8bf0552f8a33d18707e
salimovdm94-ui <salimovdm94@gmail.com>
2025-11-19 20:07:27 +0100

    Update site.config.ts
    сменил rootNotionPageId

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2b00e77eb9968186b416c6ff07c2cdd8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
