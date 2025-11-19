eed00071e51bc9c5809014b230630f2f8617dab2
salimovdm94-ui <salimovdm94@gmail.com>
2025-11-19 21:04:47 +0100

    Update next.config.js
    поправил prod-files-secure.s3.us-west-2.amazonaws.com

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ export default withBundleAnalyzer({
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'abs.twimg.com' },
       { protocol: 'https', hostname: 'pbs.twimg.com' },
-      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' }
+      { protocol: 'https', hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com' }
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
